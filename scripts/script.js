0205e14c4de10e299dc1b4b50fce9fae9f71b883
DimitrijeCvet <dimitrije.le.cvetkovic@gmail.com>
2022-04-18 09:38:06 +0200

    update

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+var myCarousel1 = document.querySelector('#carouselExampleSlidesOnly')
+var carousel = new bootstrap.Carousel(myCarousel1,{
+    interval: 3000
+})
+
+
+var myCarousel2 = document.querySelector('#carouselExampleIndicators')
+var carousel = new bootstrap.Carousel(myCarousel2,{
+    interval: 2500
+})
+
+
